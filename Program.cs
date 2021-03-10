69eed527ca3d83145660c76e10483a597f75a9a5
Natã dos Santos Silva <78987318+NatanSantos155@users.noreply.github.com>
2021-03-09 23:27:25 -0300

    Entrega da Atividade Leet

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace leet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           string frase;
+
+           Console.ForegroundColor = ConsoleColor.DarkGreen;
+           Console.WriteLine("Digite uma frase para ser traduzida para a linguagem Leet Speak");
+           Console.WriteLine("----------------------------");
+           
+           frase = Console.ReadLine();
+         
+           frase = frase.Replace("a", "4");
+           frase = frase.Replace("A", "4");
+           frase = frase.Replace("e", "3");
+           frase = frase.Replace("E", "3");
+           frase = frase.Replace("I", "1");
+           frase = frase.Replace("i", "1");
+           frase = frase.Replace("L", "1");
+           frase = frase.Replace("l", "1");
+           frase = frase.Replace("o", "0");
+           frase = frase.Replace("O", "0");
+           frase = frase.Replace("T", "7");
+           frase = frase.Replace("t", "7");
+           frase = frase.Replace("S", "5");
+           frase = frase.Replace("s", "5");
+           
+           Console.WriteLine($"{frase}");
+           
+           
+
+           
+        }
+    }
+}
